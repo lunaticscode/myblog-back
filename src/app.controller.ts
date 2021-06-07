254b2a2ee41d37ccbaa37a8daba1a08bf17b207d
박인수 <humanwater@willim.me>
2021-06-07 09:06:05 +0900

    hot-reload 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // 공통 주소
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // 세부 주소
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/articles')
+  getPost(): string{
+    return this.appService.getArticles();
   }
 }
